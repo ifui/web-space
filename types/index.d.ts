7239f5222500ca23a4803bcf212fb2567746f154
ifui <ifui@foxmail.com>
2021-02-02 08:43:39 +0800

    feat(release): v1.2.0 join anujs

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,6 @@ import { IBuildConfig } from './IBuildConfig'
 import { IPublicPath } from './IPublicPath'
 import { IPurgecssSafelist } from './IPurgecss'
 
+declare module 'es3ify-webpack-plugin'
+
 export { ISetting, IBuildPath, IBuildConfig, IPublicPath, IPurgecssSafelist }
